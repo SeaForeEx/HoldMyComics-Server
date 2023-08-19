850b7082c4c163b75cf4cca0b399d38a3863bf84
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-08-19 10:23:41 -0500

    many to many postman calls work

--- holdmycomicsapi/views/book.py
+++ holdmycomicsapi/views/book.py
@@ -2,7 +2,8 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from holdmycomicsapi.models import Book
+from rest_framework.decorators import action
+from holdmycomicsapi.models import Book, Customer, CustomerBook
 
 class BookView(ViewSet):
     """HMC Books View"""
@@ -26,6 +27,29 @@ class BookView(ViewSet):
         serializer = BookSerializer(books, many=True)
         # Return the serialized data along with the HTTP 200 OK status
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(methods=['post'], detail=True)
+    def addtocustomer(self, request, pk):
+        """Add Book To Customer"""
+        customer = Customer.objects.get(pk=request.data["customerId"])
+        book = Book.objects.get(pk=pk)
+        added = CustomerBook.objects.create(
+            customer=customer,
+            book=book
+        )
+        return Response({'message': 'Book added to Customer'}, status=status.HTTP_201_CREATED)
+    
+    @action(methods=['delete'], detail=True)
+    def removefromcustomer(self, request, pk):
+        """Remove Book From Customer"""
+        customer = Customer.objects.get(pk=request.data["customerId"])
+        book = Book.objects.get(pk=pk)
+        customer_book = CustomerBook.objects.get(
+            customer_id=customer.id,
+            book_id=book.id
+        )
+        customer_book.delete()
+        return Response({'message': 'Book removed from Customer'}, status=status.HTTP_204_NO_CONTENT)
 
 class BookSerializer(serializers.ModelSerializer):
     """JSON Serializer for Users"""
