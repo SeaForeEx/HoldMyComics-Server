cb3cb743247b2d8f4fb56c3371068b5637ea94db
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2023-09-14 00:13:31 -0500

    added phone number field to customers

--- holdmycomicsapi/models/customer.py
+++ holdmycomicsapi/models/customer.py
@@ -6,3 +6,4 @@ class Customer(models.Model):
     store_id = models.ForeignKey(User, on_delete=models.CASCADE)
     customer_name = models.CharField(max_length=100)
     email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20, default='')
